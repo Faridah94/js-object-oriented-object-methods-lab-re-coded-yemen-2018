3455d0bc31803cc6c0f186064b67b18d62aad9f9
Farida <faridah.absi@gmail.com>
2018-09-30 09:09:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 function BoardMember(name, homeState, training){
   this.name = name; 
-  this.homeState = homeState;
-  this.training = training;
-    this.veto = function (){
+  this.homeState = city;
+  this.training = depart;
+  
+  this.veto = function (){
     return 'No, I must disagree';
   }
   this.approve = function(){
