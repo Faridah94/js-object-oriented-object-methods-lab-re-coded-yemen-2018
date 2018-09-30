dee70acb648da1091d99e4566b2067792148b399
Farida <faridah.absi@gmail.com>
2018-09-30 09:09:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ function BoardMember(name, homeState, training){
   this.training = training;
     this.veto = function (){
     return 'No, I must disagree';
-  };
+  }
   this.approve = function(){
     return 'You can do that!';
-  };
+  }
   this.doCharity = function (){
     return 'I like to help people.';
-  };
+  }
   this.releasePressStatement = function(){
     return 'You will see great things from Scuber.';
   }
